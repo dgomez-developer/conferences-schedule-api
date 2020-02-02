d582681056212339381fb34d5e83de434bbe040d
Debora Gomez Bertoli <dgomez.developer@gmail.com>
2020-02-02 17:14:54 +0100

    Supporting CORS

--- server.py
+++ server.py
@@ -1,9 +1,30 @@
 import os
 import json
+import bottle
 from bottle import route, run, template, request, post, BaseResponse, response
 
-@route('/api/<conference>/schedule/<year>')
+
+# the decorator
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
+
+
+app = bottle.app()
+
+
+@app.route('/api/<conference>/schedule/<year>')
+@enable_cors
 def getConferenceSchedule(conference,year):
     return open(conference + '/'+ year + '/schedule.json')
 
-run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
