c70a49f9071958377006975ea18dcafc98d6ed95
Debora Gomez <Debora.Gomez@mytoys.de>
2020-02-13 19:38:36 +0100

    Migrating to python3

--- server.py
+++ server.py
@@ -1,30 +1,11 @@
 import os
 import json
-import bottle
 from bottle import route, run, template, request, post, BaseResponse, response
 
-
-# the decorator
-def enable_cors(fn):
-    def _enable_cors(*args, **kwargs):
-        # set CORS headers
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
-
-        if bottle.request.method != 'OPTIONS':
-            # actual request; reply with the actual response
-            return fn(*args, **kwargs)
-
-    return _enable_cors
-
-
-app = bottle.app()
-
-
-@app.route('/api/<conference>/schedule/<year>')
-@enable_cors
+@route('/api/<conference>/schedule/<year>')
 def getConferenceSchedule(conference,year):
-    return open(conference + '/'+ year + '/schedule.json')
+    response.content_type = 'application/json; charset=utf-8'
+    body = open(conference + '/'+ year + '/schedule.json')
+    return BaseResponse(body, 200, None)
 
-app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
