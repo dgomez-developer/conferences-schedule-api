0374544a57ea80b991066609c3909f0e6b1de5ae
Debora Gomez <Debora.Gomez@mytoys.de>
2020-02-14 08:24:51 +0100

    Enabling CORS for web client

--- server.py
+++ server.py
@@ -1,8 +1,27 @@
 import os
 import json
+import bottle
 from bottle import route, run, template, request, post, BaseResponse, response
 
-@route('/api/<conference>/schedule/<year>')
+# the decorator
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
+
+
+app = bottle.app()
+
+@app.route('/api/<conference>/schedule/<year>', method=['OPTIONS','GET'])
+@enable_cors
 def getConferenceSchedule(conference,year):
     response.content_type = 'application/json; charset=utf-8'
     body = open(conference + '/'+ year + '/schedule.json')
